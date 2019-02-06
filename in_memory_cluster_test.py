fab21e185b2571c2f1311177652dd44b8abb19ed
Jelly Choi <chm073@gmail.com>
2019-02-06 23:23:35 +0900

    Update test code

--- in_memory_cluster_test.py
+++ in_memory_cluster_test.py
@@ -1,33 +1,55 @@
 from collections import Counter
 from in_memory_cluster import InMemoryCluster
 
-
-i = InMemoryCluster()
-i.flush_all()
-d = {
-    "key1": ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'],
-    "key2": ['c', 'd', 'e', 'f', 'g', 'j', 'k'],
-    "key3": ['j', 'x', 'y', 'z'],
-    "key4": ['a', 'b'],
-    "key5": ['a']
+# Data structure is dictionary
+data = {
+    "user1": ['airplane', 'banana', 'cat', 'dog', 'elephant', 'fruit', 'google', 'hobby', 'internet', 'jogging'],
+    "user2": ['cat', 'dog', 'elephant', 'fruit', 'google', 'jogging', 'kotlin'],
+    "user3": ['java', 'rx', 'yahoo', 'zoo'],
+    "user4": ['apple', 'banana'],
+    "user5": ['airplane'],
+    "user6": ['bobby', 'dog'],
+    "user7": ['train', 'cat', 'exercise', 'healthy'],
+    "user8": ['healthy', 'dog', 'exercise', 'banana', 'youtube'],
+    "user9": ['java', 'javascript', 'rx', 'zoo', 'yahoo', 'google', 'github'],
+    "user10": ['cook', 'bobby', 'dog', 'youtube'],
+    "user11": ['dance', 'airplane', 'trip', 'elephant', 'fruit', 'google']
 }
 
-i.init_cluster(d)
-objs = i.load_minhash_objs(['key1', 'key2', 'key3', 'key4', 'key5'])
-for obj in objs:
-    ssi = i.search_secondary_index(obj)
-    print(Counter(ssi).most_common(10))
-    print(ssi)
+# Initialize instance
+imc = InMemoryCluster(
+    minhash_host='localhost:6379',
+    secondary_index_host='localhost:6379',
+    minhash_db=1,
+    secondary_index_db=2,
+    seed=1
+)
 
-update_data = {'key1': ['h', 'i', 'j'], 'key5': ['h', 'i', 'j', 'b', 'c']}
+# Flush minhash & secondary index db on redis
+# In example db 1 & 2
+imc.flush_all()
 
-i.update_cluster(update_data)
+# init_cluster method run next ways.
+# 1. Generate minhash by data and save on redis(db 1).
+# 2. Load minhash objects and generate secondary index.
+# 3. put in each secondary index.(db 2)
+imc.init_cluster(data)
 
-print('==========================')
+# Load minhash objects. These are instance of datasketch's MinHash object.
+users = data.keys()
+for user in users:
+    print(user, imc.most_common(user, count=10))
 
-objs = i.load_minhash_objs(['key1', 'key2', 'key3', 'key4', 'key5'])
-for obj in objs:
-    ssi = i.search_secondary_index(obj)
-    print(Counter(ssi).most_common(10))
-    print(ssi)
+# You don't need care about duplicated!
+update_data = {'user1': ['airplane', 'banana', 'cat'], 'user5': ['hobby', 'internet', 'jogging', 'banana', 'cat', 'dog']}
 
+# You should use update_cluster method after init_cluster
+# init_cluster is faster more than update_cluster
+# So use init_cluster when put your first data.
+imc.update_cluster(update_data)
+
+print('======== UPDATED!! =========')
+
+# You can check user 5 more similer than before.
+for user in users:
+    print(user, imc.most_common(user, count=10))
